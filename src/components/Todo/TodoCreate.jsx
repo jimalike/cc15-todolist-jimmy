7edb578eac67d8fcdb090e2f02d8cc7dbe8f75a0
Supanut <jimsupanut@gmail.com>
2024-03-15 03:04:13 +0700

    state:condition render

--- src/components/Todo/TodoCreate.jsx
+++ src/components/Todo/TodoCreate.jsx
@@ -1,15 +1,64 @@
-import styles from './TodoCreate.module.scss';
+
 import { FaPlus } from 'react-icons/fa';
 import { HiPlus } from 'react-icons/hi';
+import { useState } from 'react';
 
+
+import TodoForm from './TodoForm';
+import styles from './TodoCreate.module.scss';
+
+
+/*
+Condition Rendering
+- Default : Show Button &Text
+- Active : Show TodoForm
+
+Concept : true ? <AddTasl/> : <TodoForm/>
+*/
+
+/*
+  - EVENT HANDLING
+  เอ่าฟังก์ชันไปผูกติดกับ UI เพื่อให้ USER เป็นคนเรียกใช้ฟังก์ชันเอง
+  - OnClock : ต้อง Click ก่อน , FN ถึงจะรัน
+    - User ทำการคลิก
+    - Browser ทำการเรียกใช้ฟังก์ชัน
+      handleClick(eventObject)
+*/
+
+// CC3 - JS Value ไม่้สามารถทำให้ React Rerender ได้
+// ต้องใช้ State
+
+
+/*
+  CC4 - React State
+    = useState(initialState:any)
+    //  element 1 : current State
+    //  element 2 : Fn สำหรับ SetState
+*/
+//  #1 : FC = Function Component (Render)
 function TodoCreate() {
+  const [isOpenForm, setisOpenForm] = useState(false)
+  // #2 : JS Function (Logic)
+  const handleClick = function (event) {
+    setisOpenForm(!isOpenForm)
+
+
+  }
   return (
-    <div className={styles.todo__create}>
-      <div className={styles.todo__create__button}>
-        <HiPlus />
-      </div>
-      <h3 className={styles.todo__create__text}>Add Task</h3>
-    </div>
+    <>
+
+      {isOpenForm ? (
+        <TodoForm />
+      ) : (<div
+        className={styles.todo__create}
+        onClick={handleClick}
+      >
+        <div className={styles.todo__create__button}>
+          <HiPlus />
+        </div>
+        <h3 className={styles.todo__create__text}>Add Task</h3>
+      </div>)}
+    </>
   );
 }
 
