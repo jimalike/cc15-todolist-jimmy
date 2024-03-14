b935e3fe77a9e6d6478a74ca3afb09de36cc2afc
Supanut <jimsupanut@gmail.com>
2024-03-14 23:16:31 +0700

    add Todolists

--- src/component/Todo/TodoLists.jsx
+++ src/component/Todo/TodoLists.jsx
@@ -1,13 +1,25 @@
 import styles from './TodoLists.module.scss'
+import { FaTrashAlt, FaPen, FaRegCircle } from 'react-icons/fa';
 
 function TodoLists() {
-  return (
-    <div>
-      <div> todo - 1</div>
-      <div> todo - 2</div>
-      <div> todo - 3</div>
-    </div>
-  )
+    return (
+        <ul className={styles.todo__lists}>
+            <li className={styles.todo}>
+                <span className={styles.todo__checkbox}>
+                </span>
+                <p className={styles.todo__task}>TodoItem 1</p>
+                <span className={styles.todo__date}>30 AUG</span>
+                <div className={styles.todo__action}>
+                    <span className={styles.todo__edit}>
+                        <FaPen/>
+                    </span>
+                    <span className={styles.todo__delete}>
+                        <FaTrashAlt/>
+                    </span>
+                </div>
+            </li>
+        </ul>
+    )
 }
 
 export default TodoLists;
